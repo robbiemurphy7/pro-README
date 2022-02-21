ba7a55ef92116f9971500ddea20203a12752de53
robbiemurphy <rmurphy987@gmail.com>
2022-02-20 23:49:00 -0600

    README questions implemented

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use:'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List your collaberators, if any:'
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Let other developers know what they can and cannot do with your project:'
+        }
+    ]);
+};
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -12,3 +47,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+promptUser();
